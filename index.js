ee8caeb976d146bb112641193cbc6c162e8bae7b
daniel.arisa@yahoo.com <daaniel.arisa@yahoo.com>
2022-07-23 23:29:47 -0500

    commit tree aa9ec553060f63a1ae454bb4e2f21c7b74e0ac52

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// code your solution here
+// The function should receive 1 argument, an Array of JavaScript Objects
+function superbowlWin(record)
+// Each object has two properties: year and result
+// It should use find() to test each Object to see if the result is "W" — a win!
+{let result = record.find(record => record.result === "W")
+// It should return the year when the win occurred (if it occurred at all!)
+if(result){return result.year;}
+// If no win is found, it should return, sadly, undefined
+else {return undefined }
+}
